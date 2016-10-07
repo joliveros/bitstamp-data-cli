fa25867d90bcacd224c3fa894a0a0df302da7bf6
José Oliveros <jose.oliveros.1983@gmail.com>
2016-10-06 23:13:57 -0500

    lint

--- src/OrderBookStream.spec.js
+++ src/OrderBookStream.spec.js
@@ -1,14 +1,14 @@
 import chai, { expect } from 'chai';
 import chaiStream from 'chai-stream-es6';
-import nock from 'nock';
-import path from 'path';
+// import nock from 'nock';
+// import path from 'path';
 import orderBookStream from './OrderBookStream';
 
 // chai assertions for streams
 chai.use(chaiStream);
 
 // load mocks for http requests
-// const nocks = nock.load(path.resolve(__dirname, '../data/nock.json')); // eslint-disable-line no-unused-vars
+// const nocks = nock.load(path.resolve(__dirname, '../data/nock.json'));
 
 describe('OrderBookStream', () => {
   it('should return a valid readable stream', () => {
--- src/TransactionStream.spec.js
+++ src/TransactionStream.spec.js
@@ -1,14 +1,14 @@
 import chai, { expect } from 'chai';
 import chaiStream from 'chai-stream-es6';
-import nock from 'nock';
-import path from 'path';
+// import nock from 'nock';
+// import path from 'path';
 import orderBookStream from './TransactionStream';
 
 // chai assertions for streams
 chai.use(chaiStream);
 
 // load mocks for http requests
-// const nocks = nock.load(path.resolve(__dirname, '../data/nock.json')); // eslint-disable-line no-unused-vars
+// const nocks = nock.load(path.resolve(__dirname, '../data/nock.json'));
 
 describe('TransactionStream', () => {
   it('should return a valid readable stream', () => {
