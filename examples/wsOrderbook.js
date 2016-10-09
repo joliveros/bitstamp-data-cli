16d31746656f1bfba575653947b1fd4046f7f6ac
José Oliveros <jose.oliveros.1983@gmail.com>
2016-10-08 20:07:36 -0500

    update orderbook stream example

--- examples/wsOrderbook.js
+++ examples/wsOrderbook.js
@@ -1,13 +1,5 @@
-import WsOrderBookStream from '../src/wsOrderBookStream';
+import WsOrderBookStream from '../src/WsOrderBookStream';
 
 const orderBookStream = new WsOrderBookStream();
 
 orderBookStream.pipe(process.stdout);
-
-orderBookStream.on('data', (data) => {
-  const _data = JSON.parse(data);
-  
-  const now = (new Date()).getTime() / 1000;
-
-  console.log(Math.round((now - _data.timestamp) * 1000))
-});
